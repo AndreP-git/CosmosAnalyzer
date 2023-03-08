478c046966200b0c1537159d846656ee0e5bd7c4
andrioluss <andrea.unibo@gmail.com>
2023-03-09 00:10:45 +0100

    analyze_transactions ok

--- analysis/analyze_transactions_graph.py
+++ analysis/analyze_transactions_graph.py
@@ -11,150 +11,6 @@ from argparse import ArgumentParser
 import dill as pickle
 
 
-
-parser = ArgumentParser()
-
-# get day argument
-parser.add_argument("-c", "--config", dest="config", type=str, help="configuration file path")
-parser.add_argument("-d", "--day", dest="day", type=str, help="day")
-
-args = parser.parse_args()
-
-if args.config:
-    config_path = args.config
-else:
-    sys.exit("Please give a configuration file")
-
-if args.day:
-    day = args.day
-else:
-    sys.exit("Please give a day")
-
-
-#%%
-##--------------- READ CONFIGURATION FILE ----------------##
-##--------------------------------------------------------##
-
-
-with open(config_path, 'r') as f:
-    config = json.load(f)
-
-directed_graph = config['directed_graph']
-weighted_graph = config['weighted_graph']
-clustering = config['clustering']
-fraction_samples = config['fraction_samples']
-
-num_cpus = config['num_cpus']
-if num_cpus==0:
-    num_cpus = mp.cpu_count()
-    num_processes = 2 * num_cpus
-elif num_cpus==-1:
-    num_cpus = mp.cpu_count()
-    num_processes = num_cpus - 1
-else:
-    num_processes = num_cpus
-
-
-#%%
-##---------------------- READ FILE -----------------------##
-##--------------------------------------------------------##
-
-# file_path = "./data/merged_data/" + day + "_merged.txt"
-# output_path = './results/cosmos' + day
-
-# TEST VALUES
-file_path = "./data/test/00.txt"
-output_path = './results/00.txt'
-
-transactions = pd.read_csv(file_path, sep = '\s+')
-
-#%%
-##----------------- MAP PUBLIC KEY TO ID -----------------##
-##--------------------------------------------------------##
-
-nodes = pd.concat([transactions['Key-sender'], transactions['Key-receiver']]).unique()
-
-nodes_dict = {address:i for i,address in enumerate(nodes)}
-
-transactions['id_sender'] = [nodes_dict[address] for address in transactions['Key-sender']]
-transactions['id_receiver'] = [nodes_dict[address] for address in transactions['Key-receiver']]
-
-# no self-loops
-transactions = transactions[transactions.id_sender != transactions.id_receiver]
-
-
-#%%
-##------------------- BUILD DATA GRAPH -------------------##
-##--------------------------------------------------------##
-
-if weighted_graph:
-    output_path += '_weighted'
-    transactions_weighted = transactions.groupby(transactions.columns.tolist(), as_index=False).size()
-    if directed_graph:
-        output_path += '_directed'
-        data_graph = nx.from_pandas_edgelist(transactions_weighted, 'id_sender', 'id_receiver', 'size', create_using=nx.DiGraph)
-    else:
-        data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.Graph)
-else:
-    if directed_graph:
-        output_path += '_directed'
-        data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.DiGraph)
-    else:
-        data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.Graph)
-
-n_nodes_data = data_graph.number_of_nodes()
-n_edges_data = data_graph.number_of_edges()
-
-print("Created graph for the day {}.".format(day))
-if directed_graph:
-    print("The graph is directed")
-else:
-    print("The graph is undirected")
-if weighted_graph:
-    print("The graph is weighted")
-else:
-    print("The graph is unweighted")
-print("Number of nodes of data graph: {}".format(n_nodes_data))
-print("Number of edges of data graph: {}\n".format(n_edges_data))
-
-
-#%%
-##---------------------- PREPARE OUTPUT FILE -----------------------##
-##------------------------------------------------------------------##
-
-output_path += '_transactions'
-
-if os.path.isfile(output_path + '.json'):
-    print("Found already existing results. Trying to read old data and append new ones.\n")
-
-    with open(output_path + '.json') as f:
-        data = json.loads(f.read())
-
-else:
-    data = {'metadata': {'day': day,
-                        'directed': directed_graph,
-                        'weighted': weighted_graph,},
-            'number_of_nodes': n_nodes_data,
-            'number_of_edges': n_edges_data,
-            'clustering_coefficient': None,
-            'num_cpus': [],
-            'sample_size': [],
-            'num_links': [],
-            'time': [],
-            'ASPL': []}
-
-
-# %%
-##---------------- CLUSTERING COEFFICIENT ----------------##
-##--------------------------------------------------------##
-
-if clustering:
-
-    clust_coeff_data = nx.average_clustering(data_graph)
-    print("Clustering coefficient of data graph: {:.8f}\n".format(clust_coeff_data))
-    data['clustering_coefficient'] = clust_coeff_data
-
-
 # %%
 ##---------------- AVERAGE SHORTEST PATH -----------------##
 ##--------------------------------------------------------##
@@ -173,12 +29,154 @@ def ASPL(nodes, graph, return_dict, procnum):
 
     return_dict[procnum] = (tot_SPL, tot_paths)
 
-
-#%%
-##--------------------- ASPL DATA ------------------------##
-##--------------------------------------------------------##ù
-
 if __name__ == '__main__':
+
+    parser = ArgumentParser()
+
+    # get day argument
+    parser.add_argument("-c", "--config", dest="config", type=str, help="configuration file path")
+    parser.add_argument("-d", "--day", dest="day", type=str, help="day")
+
+    args = parser.parse_args()
+
+    if args.config:
+        config_path = args.config
+    else:
+        sys.exit("Please give a configuration file")
+
+    if args.day:
+        day = args.day
+    else:
+        sys.exit("Please give a day")
+
+
+    #%%
+    ##--------------- READ CONFIGURATION FILE ----------------##
+    ##--------------------------------------------------------##
+
+
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+
+    directed_graph = config['directed_graph']
+    weighted_graph = config['weighted_graph']
+    clustering = config['clustering']
+    fraction_samples = config['fraction_samples']
+
+    num_cpus = config['num_cpus']
+    if num_cpus==0:
+        num_cpus = mp.cpu_count()
+        num_processes = 2 * num_cpus
+    elif num_cpus==-1:
+        num_cpus = mp.cpu_count()
+        num_processes = num_cpus - 1
+    else:
+        num_processes = num_cpus
+
+
+    #%%
+    ##---------------------- READ FILE -----------------------##
+    ##--------------------------------------------------------##
+
+    # file_path = "./data/merged_data/" + day + "_merged.txt"
+    # output_path = './results/cosmos' + day
+
+    # TEST VALUES
+    file_path = "./data/test/00.txt"
+    output_path = './results/00.txt'
+
+    transactions = pd.read_csv(file_path, sep = '\s+')
+
+    #%%
+    ##----------------- MAP PUBLIC KEY TO ID -----------------##
+    ##--------------------------------------------------------##
+
+    nodes = pd.concat([transactions['Key-sender'], transactions['Key-receiver']]).unique()
+
+    nodes_dict = {address:i for i,address in enumerate(nodes)}
+
+    transactions['id_sender'] = [nodes_dict[address] for address in transactions['Key-sender']]
+    transactions['id_receiver'] = [nodes_dict[address] for address in transactions['Key-receiver']]
+
+    # no self-loops
+    transactions = transactions[transactions.id_sender != transactions.id_receiver]
+
+
+    #%%
+    ##------------------- BUILD DATA GRAPH -------------------##
+    ##--------------------------------------------------------##
+
+    if weighted_graph:
+        output_path += '_weighted'
+        transactions_weighted = transactions.groupby(transactions.columns.tolist(), as_index=False).size()
+        if directed_graph:
+            output_path += '_directed'
+            data_graph = nx.from_pandas_edgelist(transactions_weighted, 'id_sender', 'id_receiver', 'size', create_using=nx.DiGraph)
+        else:
+            data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.Graph)
+    else:
+        if directed_graph:
+            output_path += '_directed'
+            data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.DiGraph)
+        else:
+            data_graph = nx.from_pandas_edgelist(transactions, 'id_sender', 'id_receiver', create_using=nx.Graph)
+
+    n_nodes_data = data_graph.number_of_nodes()
+    n_edges_data = data_graph.number_of_edges()
+
+    print("Created graph for the day {}.".format(day))
+    if directed_graph:
+        print("The graph is directed")
+    else:
+        print("The graph is undirected")
+    if weighted_graph:
+        print("The graph is weighted")
+    else:
+        print("The graph is unweighted")
+    print("Number of nodes of data graph: {}".format(n_nodes_data))
+    print("Number of edges of data graph: {}\n".format(n_edges_data))
+
+
+    #%%
+    ##---------------------- PREPARE OUTPUT FILE -----------------------##
+    ##------------------------------------------------------------------##
+
+    output_path += '_transactions'
+
+    if os.path.isfile(output_path + '.json'):
+        print("Found already existing results. Trying to read old data and append new ones.\n")
+
+        with open(output_path + '.json') as f:
+            data = json.loads(f.read())
+
+    else:
+        data = {'metadata': {'day': day,
+                            'directed': directed_graph,
+                            'weighted': weighted_graph,},
+                'number_of_nodes': n_nodes_data,
+                'number_of_edges': n_edges_data,
+                'clustering_coefficient': None,
+                'num_cpus': [],
+                'sample_size': [],
+                'num_links': [],
+                'time': [],
+                'ASPL': []}
+
+
+    # %%
+    ##---------------- CLUSTERING COEFFICIENT ----------------##
+    ##--------------------------------------------------------##
+
+    if clustering:
+
+        clust_coeff_data = nx.average_clustering(data_graph)
+        print("Clustering coefficient of data graph: {:.8f}\n".format(clust_coeff_data))
+        data['clustering_coefficient'] = clust_coeff_data
+
+
+    #%%
+    ##--------------------- ASPL DATA ------------------------##
+    ##--------------------------------------------------------##
 
     for fs in fraction_samples:
 
